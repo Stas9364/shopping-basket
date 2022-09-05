f4fa2403b80e3642275f6055b23bef42bc492573
Stas9364 <stas-lisovskii@mail.ru>
2022-09-05 13:13:29 +0300

    commit

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -19,15 +19,6 @@ import {
     query
 } from "firebase/firestore";
 
-// export const fbConfig = {
-//     apiKey: 'AIzaSyCzwR0-KqRLBCndHE0p6hnw0rwdsTBW_Lo',
-//     authDomain: 'shopping-basket-71702.firebaseapp.com',
-//     databaseURL: 'https://shopping-basket-71702-default-rtdb.europe-west1.firebasedatabase.app',
-//     projectId: 'shopping-basket-71702',
-//     storageBucket: 'shopping-basket-71702.appspot.com',
-//     messagingSenderId: '1095465613430',
-//     appId: '1:1095465613430:web:db4b417b368ced5085c757'
-// };
 export const fbConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
